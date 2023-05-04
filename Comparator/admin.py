6578a0474c607858c4826f783ec42c3711098946
Carlosdb148 <92909837+Carlosdb148@users.noreply.github.com>
2023-05-04 10:51:38 +0200

    Add files via upload

--- Comparator/admin.py
+++ Comparator/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Product, Shop, Stock
 
 # Register your models here.
+admin.site.register(Product)
+# class RequestDemoAdmin(admin.ModelAdmin):
+#   list_display = [field.name for field in
+# Product._meta.get_fields()]
+
+admin.site.register(Shop)
+
+admin.site.register(Stock)
