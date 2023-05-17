f9d4a439a313f4bc8cf5e68511269e19ef1b611d
JuanMartiin <92909826+JuanMartiin@users.noreply.github.com>
2023-05-17 13:48:27 +0200

    version1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { ComparadorGrid } from "./components/ComparadorGrid";
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+import { ProductDetails } from "./pages/ProductDetails";
+import { LandingPages } from "./pages/LandingPages";
+import { Aviso } from "./pages/Aviso";
+import { Privacidad } from "./pages/Privacidad";
+import { Cookies } from "./pages/Cookies";
 
-function App() {
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <header className="navi">
+        <Link to="/" className="navHome">Home</Link>
+        <Link to="/producto" className="navProd">Producto</Link>
       </header>
-    </div>
+      <Routes>
+        <Route exact path="/producto" element={<ProductDetails />}></Route>
+        <Route path="/" element={<LandingPages />}></Route>
+        <Route path="/aviso" element={<Aviso />}></Route>
+        <Route path="/privacidad" element={<Privacidad />}></Route>
+        <Route path="/cookies" element={<Cookies />}></Route>
+      </Routes>
+      <footer className="footer">
+          <div className="separador"></div>
+          <div className="flex">
+            <Link to="/aviso" class="aviso">Aviso Legal</Link>
+            <Link to="/privacidad" class="privacidad">Política de privacidad</Link>
+            <Link to="/cookies" class="cookies">Política de cookies</Link>
+          </div>
+      </footer>
+    </Router>
   );
 }
-
-export default App;
