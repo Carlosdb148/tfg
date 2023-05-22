a6e8398571b1b2aa840f48c53e072afc73c8c7c9
JuanMartiin <jimartin125@ieszaidinvergeles.org>
2023-05-22 11:42:37 +0200

    version2

--- src/components/ProductoCard.jsx
+++ src/components/ProductoCard.jsx
@@ -1,12 +1,15 @@
+import {Link} from "react-router-dom";
 import styles from "./ProductoCard.module.css"
 
 export function ProductoCard({ producto }) {
   return (
     <li className={styles.productoCard}>
+      <Link to={"/producto/" + producto.id}>
       <div className={styles.tarjeta}>
         <div className={styles.imagen}/>
         <div className={styles.nombre}>{producto.nombre}</div>
       </div>
+      </Link>
   </li>
   )
 }
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,3 +1,35 @@
+import producto from "./producto.json";
+import styles from "./ProductDetails.module.css";
+
 export function ProductDetails(){
-    return <div>Detalles</div>;
+    return <div>
+        {/* <img src={producto.foto} alt=""/> */}
+        <div className={styles.detailsContainer}>
+            <div className={styles.imagen}/>
+            <div>
+                <p className={styles.nombre}>{producto.nombre}</p>
+                <p className={styles.descripcion}>{producto.descripcion}</p>
+            </div>
+        </div>
+        <div className={styles.precioM}>
+            <div className={styles.Mtitulo}>Media Markt</div>
+            <div className={styles.Mprecio}>{producto.precioMediaMarkt}€</div>
+            <div className={styles.Murl}><a href={producto.urlMediaMarkt}>Comprar</a></div>
+        </div>
+        <div className={styles.precioA}>
+            <div className={styles.Mtitulo}>Amazon</div>
+            <div className={styles.Mprecio}>{producto.precioAmazon}€</div>
+            <div className={styles.Murl}><a href={producto.urlAmazon}>Comprar</a></div>
+        </div>
+        <div className={styles.precioP}>
+            <div className={styles.Mtitulo}>Phone House</div>
+            <div className={styles.Mprecio}>{producto.precioPhoneHouse}€</div>
+            <div className={styles.Murl}><a href={producto.urlPhoneHouse}>Comprar</a></div>
+        </div>
+        <div className={styles.precioW}>
+            <div className={styles.Mtitulo}>Worten</div>
+            <div className={styles.Mprecio}>{producto.precioWorten}€</div>
+            <div className={styles.Murl}><a href={producto.urlWorten}>Comprar</a></div>
+        </div>
+    </div>;
 }
