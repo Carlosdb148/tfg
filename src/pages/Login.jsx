bc88c9a03416b20b72250f347d369cb91fd4e7a3
JuanMartiin <jimartin125@ieszaidinvergeles.org>
2023-05-25 08:32:36 +0200

    funcionalidad registro y login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,38 @@
-export function Login(){
-    return (
-        <div>
-            <form className="formLogin">
-                <p className="tituloRegistro">Logueate en Traziem Technology</p>
-                <input type="text" className="loginUsuario" placeholder="Usuario"></input>
-                <input type="password" className="loginContraseña" placeholder="Contraseña"></input>
-                <input type="submit" className="registroBtn"></input>
-            </form>
-        </div>
-    );
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export function Login() {
+  const [usuario, setUsuario] = useState('');
+  const [contraseña, setContraseña] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const formData = {
+      username: usuario,
+      password: contraseña
+    };
+
+    axios.post('http://localhost:8000/api/login', formData)
+      .then(response => {
+        const token = response.data.token;
+        // Aquí puedes manejar el token recibido, por ejemplo, almacenarlo en el almacenamiento local (localStorage)
+        localStorage.setItem('token', token);
+        console.log('Inicio de sesión exitoso');
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  return (
+    <div>
+      <form className="formLogin" onSubmit={handleSubmit}>
+        <p className="tituloRegistro">Logueate en Traziem Technology</p>
+        <input type="text" className="loginUsuario" placeholder="Usuario" value={usuario} onChange={(e) => setUsuario(e.target.value)} />
+        <input type="password" className="loginContraseña" placeholder="Contraseña" value={contraseña} onChange={(e) => setContraseña(e.target.value)} />
+        <input type="submit" className="registroBtn" />
+      </form>
+    </div>
+  );
 }
--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -1,40 +1,51 @@
 import axios from 'axios';
 
-export function Registro(){
-    
-    const handleSubmit = (event) => {
-        event.preventDefault();
-    
-        const { nombre, apellidos, email, usuario, contraseña } = event.target.elements;
-    
-        const formData = {
-          first_name: nombre.value,
-          last_name: apellidos.value,
-          email: email.value,
+export function Registro() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const { nombre, apellidos, email, usuario, contraseña } = event.target.elements;
+
+    const formData = {
+      first_name: nombre.value,
+      last_name: apellidos.value,
+      email: email.value,
+      username: usuario.value,
+      password: contraseña.value
+    };
+
+    axios.post('http://localhost:8000/api/register', formData)
+      .then(response => {
+        console.log(response.data);
+        // Si el registro fue exitoso, realizar el inicio de sesión
+        const loginData = {
           username: usuario.value,
           password: contraseña.value
         };
-    
-        axios.post('http://localhost:8000/api/register', formData)
-          .then(response => {
-            console.log(response.data);
-          })
-          .catch(error => {
-            console.error(error);
-          });
-      };
+        return axios.post('http://localhost:8000/api/login', loginData);
+      })
+      .then(response => {
+        const token = response.data.token;
+        // Aquí puedes manejar el token recibido, por ejemplo, almacenarlo en el almacenamiento local (localStorage)
+        localStorage.setItem('token', token);
+        console.log('Inicio de sesión exitoso');
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
 
-    return (
-        <div>
-            <form className="formRegistro" onSubmit={handleSubmit}>
-                <p className="tituloRegistro">Registrate en Traziem Technology</p>
-                <input type="text" className="registroNombre" placeholder="Nombre" name="nombre" id="nombre"></input>
-                <input type="text" className="registroApellido" placeholder="Apellidos" name="apellidos" id="apellidos"></input>
-                <input type="email" className="registroEmail" placeholder="Email" name="email" id="email"></input>
-                <input type="text" className="registroUsuario" placeholder="Usuario" name="usuario" id="usuario"></input>
-                <input type="text" className="registroContraseña" placeholder="Contraseña" name="contraseña" id="contraseña"></input>
-                <input type="submit" className="registroBtn"></input>
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      <form className="formRegistro" onSubmit={handleSubmit}>
+        <p className="tituloRegistro">Registrate en Traziem Technology</p>
+        <input type="text" className="registroNombre" placeholder="Nombre" name="nombre" />
+        <input type="text" className="registroApellido" placeholder="Apellidos" name="apellidos" />
+        <input type="email" className="registroEmail" placeholder="Email" name="email" />
+        <input type="text" className="registroUsuario" placeholder="Usuario" name="usuario" />
+        <input type="text" className="registroContraseña" placeholder="Contraseña" name="contraseña" />
+        <input type="submit" className="registroBtn" />
+      </form>
+    </div>
+  );
 }
