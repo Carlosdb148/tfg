990c3a3c8bc87c503a07c480404354f32b28147f
Carlosdb148 <cdiaz311@ieszaidinvergeles.org>
2023-05-29 09:26:17 +0200

    Fixed: register route

--- Comparator/views.py
+++ Comparator/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework import generics, permissions, mixins
 from django.contrib.auth.models import User
 from rest_framework.permissions import  IsAdminUser, IsAuthenticated
+from rest_framework.views import APIView
 
 
 from bs4 import BeautifulSoup
@@ -43,14 +44,13 @@ class GetProducts(generics.GenericAPIView):
     
 
 #Register API
-class RegisterApi(generics.GenericAPIView):
+class RegisterApi(APIView):
     def post(self, request, *args,  **kwargs):
-        serializer = self.get_serializer(data=request.data)
+        serializer = RegisterSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.is_active = True
         user = serializer.save()
         return Response({
-            "user": UserSerializer(user,    context=self.get_serializer_context()).data,
             "message": "User Created Successfully.  Now perform Login to get your token",
         })
     
