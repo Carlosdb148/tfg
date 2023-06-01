1402a8aa57dee4007b0449cd5966dee7d2306031
JuanMartiin <jimartin125@ieszaidinvergeles.org>
2023-06-01 12:06:05 +0200

    show img

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -30,10 +30,30 @@ export function ProductDetails() {
     return null;
   }
 
+  // const decodeBase64Image = (base64Image) => {
+  //   const imageData = atob(base64Image.split(",")[1]); // Obtener los datos binarios de la imagen
+  //   const arrayBuffer = new ArrayBuffer(imageData.length);
+  //   const uintArray = new Uint8Array(arrayBuffer);
+
+  //   for (let i = 0; i < imageData.length; i++) {
+  //     uintArray[i] = imageData.charCodeAt(i);
+  //   }
+
+  //   const blob = new Blob([arrayBuffer], { type: "image/jpeg" });
+  //   const imageUrl = URL.createObjectURL(blob);
+  //   return imageUrl;
+  // };
+
+  // const imageUrl = decodeBase64Image(producto.foto);
+
+  console.log(producto);
   return <div>
         {/* <img src={producto.foto} alt=""/> */}
         <div className={styles.detailsContainer}>
-            <div className={styles.imagen}/>
+            {/* <div className={styles.imagen}/> */}
+          {/* <div className={styles.contenedorImg}>
+            <img className={styles.imagen} src={imageUrl} alt={producto.nombre} />
+          </div> */}
             <div>
                 <p className={styles.nombre}>{producto.nombre}</p>
                 <p className={styles.descripcion}>{producto.descripcion}</p>
